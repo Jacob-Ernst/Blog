2cabf208bb860cfdc6bbd12af1751d655dc4acea
Jacob Ernst <akumadude.je@gmail.com>
2014-10-03 11:17:36 -0500

    Created a rolldice route and made a random number display in the view. One can guess the number and it will display a message in html telling you if you guessed correctly

--- app/routes.php
+++ app/routes.php
@@ -16,9 +16,23 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
-Route::get('/resume', function(){
+Route::get('/resume/{who}', function($isemployer){
+    if ($isemployer = 'yes') {
+        return Redirect::to('http://google.com');
+    }
     return "Este es mi resume, brutha!";
 });
+
+Route::get('/rolldice/{guess}', function($guess)
+{
+    $random = rand(1, 6);
+    $data = [
+        'random' => $random,
+        'guess' => $guess
+    ];
+    return View::make('roll-dice', $data);
+});
+
 
 Route::get('/portfolio', function(){
     return "Este es mi portfolio, brutha!";
---
+++ app/views/roll-dice.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Gamblin'</title>
+</head>
+<body>
+    <h1><?php echo $random; ?></h1>
+    <?php if($guess == $random):?>
+        <h2>Good Guess</h2>
+    <?endif;?>
+</body>
+</html>
