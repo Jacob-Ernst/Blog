d93e464d98a95ceb9ce66227a3dbff43b37efab2
Jacob Ernst <akumadude.je@gmail.com>
2014-10-07 11:51:25 -0500

    Fixed labels for the inputs.

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
 <form action="{{{ action('PostsController@store') }}}" method='POST'>
-    <label for='title'></label>
+    <label for='title'>Title:</label>
     <input type="text" name="title" placeholder="Title" id='title' value="{{{Input::old('title')}}}">
-    <label for='content'></label>
+    <label for='content'>Content:</label>
     <textarea id='content' name='content' placeholder='Content'>{{{Input::old('content')}}}</textarea>
     <input type="submit">
 </form>
