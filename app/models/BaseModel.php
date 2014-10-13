72a1bbea0a309a8678e1a6120b9115d93c6138e5
Jacob Ernst <akumadude.je@gmail.com>
2014-10-13 09:51:55 -0500

    Changed mutators and accusers for Post and User models.

---
+++ app/models/BaseModel.php
@@ -0,0 +1,25 @@
+<?php
+    
+use Carbon\Carbon;
+
+class BaseModel extends Eloquent {
+    
+    public function getCreatedAtAttribute($value)
+    {  
+        $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+        return $utc->setTimezone('America/Chicago');
+    }
+    public function getUpdatedAtAttribute($value){
+        $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+        return $utc->setTimezone('America/Chicago');
+    }
+    public function setCreatedAtAttribute($value)
+    {  
+        $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+        return $utc->setTimezone('UTC');
+    }
+    public function setUpdatedAtAttribute($value){
+        $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+        return $utc->setTimezone('UTC');
+    }
+}
--- app/models/Post.php
+++ app/models/Post.php
@@ -1,9 +1,11 @@
 <?php
 
-class Post extends Eloquent {
+
+class Post extends BaseModel {
     public static $rules = array(
         'title' => 'required|max:255', 
         'content' => 'required'
     );
     protected $table = 'posts';
+    
 }
