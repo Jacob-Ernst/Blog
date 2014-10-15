58bc0a484314683a7d4cc10b6fc490189bc14a15
Jacob Ernst <akumadude.je@gmail.com>
2014-10-15 15:26:48 -0500

    Minor changes to the show view and addition of new package for image management intended for future implementation.

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -3,10 +3,10 @@
 
 @section('content')
 
-    <h1 class='text-left page-header'>{{{$post->title}}}    <small>{{{$post->created_at->diffForHumans()}}}</small></h1>
+    <h1 class='text-left page-header'>{{{$post->title}}}</h1>
     <img src="{{$post->file}}" class='img-responsive'>
     <p class=''>{{{$post->content}}}</p>
-    <p class='small'>Created by {{{$post->user->first_name}}} {{{$post->user->last_name}}}</p>
+    <p class='small'>Created by {{{$post->user->first_name}}} {{{$post->user->last_name}}} {{{$post->created_at->diffForHumans()}}}</p>
    
     @if(Auth::check() && Auth::Id() == $post->user_id)
         {{ Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'delete', 'id' => 'delete-form']) }}
