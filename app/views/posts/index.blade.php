1f6bbf0935df97cebb73111ada036089b450c51b
Jacob Ernst <akumadude.je@gmail.com>
2014-11-19 09:18:32 -0600

    Fixed html, displayed tags, and fixed broken image issue with posts that have no image.

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -8,6 +8,7 @@
 @stop
 
 @section('content')
+        <div class='row'>
             @if(Input::has('search'))
                 <h1>Posts containing the word(s) "{{{Input::get('search')}}}":</h1>
             @endif
@@ -17,9 +18,20 @@
                         <h3><a href="{{{ action('PostsController@show', $post->id) }}}">{{ $post->title}}</a><small> by {{{$post->user->first_name}}} {{{$post->user->last_name}}}</small></h3>
                     </div>
                         <p class='small'>created {{{$post->created_at->diffForHumans()}}}</p>
+                        @if($post->file)
                             <img src="{{$post->file}}" class='img-responsive'>
+                        @endif
                         <p>{{ substr($post->content, 0, 255) . "..."}}</p>
                         <a href="{{ action('PostsController@show', $post->id)}}" class='btn btn-primary btn-sm'>Read more</a>
+                        <ul class="nav nav-pills">
+                        @foreach ($post->tags as $tag)
+                            <li>
+                                <a href="{{{ action('PostsController@index', ['tag' => $tag->tag]) }}}">
+                                <i class="fa fa-tag"></i> {{{ $tag->tag }}}
+                                </a>
+                            </li>
+                        @endforeach
+                       </ul> 
                 </div>
                 
             @empty
@@ -31,5 +43,5 @@
         <div class="text-center">
             {{ $posts->appends(array('search' => Input::get('search')))->links()}}
         </div>
-    
+    </div>
 @stop
