8cf4008558cccf6d5dbd055842101fd6c8a313c0
Jacob Ernst <akumadude.je@gmail.com>
2014-11-21 13:26:50 -0600

    Fixed basic home view

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,7 +5,12 @@
 
     <div class="jumbotron">
         <h1>Hello, world!</h1>
-        <p>...</p>
+        <p>My Info:</p>
+        <ul>
+            <li><a href=""><i class="fa fa-linkedin-square fa-2x"></i>  LinkedIn</a></li>
+            <li><a href=""><i class="fa fa-github fa-2x"></i>   GitHub</a></li>
+            <li><a href="mailto:jacob.f.ernst@gmail.com"><i class="fa fa-envelope fa-2x"></i>   Email</a></li>
+        </ul>
         <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
     </div>
 
