d452c4caa90070f686f05d07e9f67bd55fd62115
Jacob Ernst <akumadude.je@gmail.com>
2014-12-03 10:50:29 -0600

    Editing css for resume view.

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -3,15 +3,22 @@
 @section('content')
 
 
-    <div class="jumbotron">
-      <h2 class='text-center'>Jacob Ernst <small>Full Stack Software Developer</small></h2>
-      <p class='text-center'>Contact:
-        <a href="">fsfaf</a>
-        <a href="">fasfaf</a>
-        <a href="">asfaf</a>
-      </p>
-      <p><a class="btn btn-primary btn-lg" role="button">Learn more</a></p>
+<div class="jumbotron">
+    <h2 class='text-center'>Jacob Ernst <small>Full Stack Software Developer</small></h2>
+    <p class='text-center'>Download:    
+        <a href="/JacobErnstResume.pdf" target='_blank'><i class="fa fa-file-pdf-o fa-lg"></i></a>
+        <a href="/JacobErnstResume.pdf" target='_blank'><i class="fa fa-file-word-o fa-lg"></i></a>
+    </p>
+    <div class="row">
+            <div class="col-xs-3 col-md-5 col-md-offset-4">
+                <ul >
+                    <LH class='text-left'>Contact me</LH>
+                    <li>Pikachu</li>
+                    <li>Bill</li>
+                </ul>
+            </div>
     </div>
+</div>
 
 
 
